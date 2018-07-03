ba7669f5ea05311e13f5f1ad271e83508c8664b3
murilobnt <murilo.araujo.md@gmail.com>
2018-07-03 03:50:08 -0300

    Changing sqlite file location

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/mourilo/Documentos/ufrn/processos/spotify-playlist-generator-server/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 class Token(db.Model):
