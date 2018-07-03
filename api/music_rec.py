cfbaa7316d60be692efdd2ffd2e0ec5c27309bbd
murilobnt <murilo.araujo.md@gmail.com>
2018-07-03 06:39:46 -0300

    Changing music rec method

--- api/music_rec.py
+++ api/music_rec.py
@@ -11,26 +11,25 @@ import spotipy.util as util
 from src import db
 from src import Token
 
-def get_id_music_recommendation_new_playlist(self, data):
-    list_id_recomendation = []
+calculate_audio_features = {}
+list_id_recomendation = []
 
+def get_id_music_recommendation_new_playlist(data):
     size_list = len(data['tracks'])
     for i in range(0,size_list):
         list_id_recomendation.insert(0, data['tracks'][i]['id'])
     return list_id_recomendation
 
-def set_audio_features(self, sp):
-    calculate_audio_features = {}
-
-    audio_features = sp.audio_features(self.get_top_tracks_user())
+def set_audio_features(sp):
+    audio_features = sp.audio_features(get_top_tracks_user())
     td = pd.DataFrame(audio_features)
     td.head()
 
     features = ['energy', 'acousticness', 'danceability', 'instrumentalness', 'speechiness', 'valence']
 
     for x in features:
-        self.calculate_audio_features['min_' + x] = str(td[x].mean() - td[x].var())
-        self.calculate_audio_features['max_' + x] = str(td[x].mean() + td[x].var())
+        calculate_audio_features['min_' + x] = str(td[x].mean() - td[x].var())
+        calculate_audio_features['max_' + x] = str(td[x].mean() + td[x].var())
 
     return calculate_audio_features
 
@@ -40,7 +39,7 @@ class Music_Recommendation(Resource):
             token = Token.query.get(1)
             sp = spotipy.Spotify(auth=token.token_value)
             calculate_audio_features = set_audio_features(sp)
-            list_id_recomendation = get_id_music_recommendation_new_playlist(sp.recommendations(seed_artists=None, seed_genres=['classical'], seed_tracks=None, limit=10, country=None, min_energy=self.calculate_audio_features['min_energy']))
+            list_id_recomendation = get_id_music_recommendation_new_playlist(sp.recommendations(seed_artists=None, seed_genres=['classical'], seed_tracks=None, limit=10, country=None, min_energy=calculate_audio_features['min_energy']))
             return {
                 'recommendation_list' : list_id_recomendation
             }
