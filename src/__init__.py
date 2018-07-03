93794fdb3adcbdd1740396b56fe2f42fff29a37f
murilobnt <murilo.araujo.md@gmail.com>
2018-07-03 04:11:02 -0300

    Modifying SQLAlchemy

--- src/__init__.py
+++ src/__init__.py
@@ -6,7 +6,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.db')
 db = SQLAlchemy(app)
 
 class Token(db.Model):
