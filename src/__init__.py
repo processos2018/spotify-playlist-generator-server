6ea7f526d2fb727a134fae3645178cdee376d01f
murilobnt <murilo.araujo.md@gmail.com>
2018-07-03 03:58:22 -0300

    Changing sqlite file location

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 db = SQLAlchemy(app)
 
 class Token(db.Model):
